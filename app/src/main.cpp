6357ec779ca643ae186b4001c1373badbf69d039
mzulfikari <mzulfikari1382@gmail.com>
2026-06-02 16:20:49 +0330

    Implement plugin manager and runtime architecture

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,48 +1,55 @@
 #include <iostream>
 
-#include "runtime/PluginLoader.h"
+#include "runtime/PluginManager.h"
 #include "runtime/IPlugin.h"
 
 int main()
 {
-    try
+    PluginManager manager;
+
+    if (!manager.loadPlugin("NumericModule.dll"))
     {
-        PluginLoader loader;
+        std::cerr
+            << "Failed to load plugin\n";
 
-        PluginHandle plugin =
-            loader.load("NumericModule.dll");
+        return 1;
+    }
 
+    const auto& plugins =
+        manager.getRegistry().getAll();
+
+    for (const auto& [name, plugin] : plugins)
+    {
         auto metadata =
             plugin.instance->getMetadata();
 
         std::cout << "Plugin Loaded\n";
-        std::cout << "Name: "
-                  << metadata.name
-                  << '\n';
 
-        std::cout << "Version: "
-                  << metadata.version
-                  << '\n';
-
-        std::cout << "Author: "
-                  << metadata.author
-                  << '\n';
-
-        std::cout << "Description: "
-                  << metadata.description
-                  << '\n';
-
-        loader.unload(plugin);
-
-        std::cout << "\nPlugin Unloaded\n";
-    }
-    catch (const std::exception& ex)
-    {
-        std::cerr
-            << "Error: "
-            << ex.what()
+        std::cout
+            << "Name: "
+            << metadata.name
             << '\n';
+
+        std::cout
+            << "Version: "
+            << metadata.version
+            << '\n';
+
+        std::cout
+            << "Author: "
+            << metadata.author
+            << '\n';
+
+        std::cout
+            << "Description: "
+            << metadata.description
+            << "\n\n";
     }
+
+    manager.unloadAll();
+
+    std::cout
+        << "All Plugins Unloaded\n";
 
     return 0;
 }
